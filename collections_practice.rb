b4474e54d4a5e6799611716ff33e623b4d3ce6f6
Adham Muhammad <admuaq@gmail.com>
2018-07-14 07:56:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
 # your code goes here
+def begins_with_r(argument)
+  
+  test = []
+  
+  argument.each do |element|
+      element[0]
+      if element[0] != "r"
+        return false 
+      end  
+    end 
+    return true
+end
+
+def contain_a(argument)
+container = []
+argument.collect do |element|
+  if element.include?("a")
+    container << element
+  end
+end 
+
+container
+
+end 
+
+def first_wa(argument)
+  argument.each.detect("wa") do |x|
+    
+end 
